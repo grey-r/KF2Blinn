e3fec4b3416ea4badff08b8de17a903404dc5b2b
Grey Ruessler <greyruessler@gmail.com>
2019-08-17 18:44:37 -0500

    started channel extraction

--- kf2blinn.py
+++ kf2blinn.py
@@ -9,19 +9,29 @@ except ImportError:
                 or run pip install Pillow.""")
 
 def exec(imageAr):
-    dif = imageAr[0]
-    mask = imageAr[1]
-    normal = imageAr[2]
-    spec = imageAr[3]
-
+    #get maximum sizes
     wMax = 0
     hMax = 0
     for im in imageAr:
         wMax = max(im.width,wMax)
         hMax = max(im.height,hMax)
+    
+    #scale all to maximum size
+    for im in imageAr:
+        if (im.width<wMax or im.height<hMax):
+            im.resize((wMax,hMax), resample = PIL.Image.LANCZOS)
+    
+    #extract channels
+    diffuse = imageAr[0]
+    mask = imageAr[1]
+    normal = imageAr[2]
+    specular = imageAr[3]
 
-    #do something
-    print(str(wMax) + "/" + str(hMax))
+    maskSplit = mask.split()
+    print(len(maskSplit))
+
+
+
 
 d = Image.open("./samples/WEP_1P_MAC10_TEX/Texture2D/Wep_1stP_Mac10_D.tga")
 m = Image.open("./samples/WEP_1P_MAC10_TEX/Texture2D/Wep_1stP_Mac10_M.tga")
